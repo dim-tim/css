3073abf51c7a7d919fe21e41b15262862a223ed1
Dzmitry Kislo <dzmitry.kislo@ihg.com>
2015-08-22 10:45:33 +0300

    post mail

--- mail.php
+++ mail.php
@@ -1,1 +1,12 @@
-<?php ?>
+<?php
+
+$recepient = "VseAuto_brest@mail.ru";
+$sitename = "VseAuto";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$time = trim($_POST["time"]);
+$message = "Имя: $name \nТелефон: $phone \nВремя звонка: $time";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
